0c8979d2c523abf4ed9934e3b1fd149b4bd65b0e
rhenea <rheneadianne@gmail.com>
2022-05-31 21:41:37 -0400

    figured out how to add initial timer T.T

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,31 @@
-scawy!!!!!!
+// variables
+const question = $(".question-wrap")
+const start = $(".start")
+const intro = $(".intro")
+const questionTitle = $(".question")
+const choices = $(".choices")
+const initialTimer = document.querySelector(".initialTimer")
+
+// hides intro, countsdown 3 seconds, then shows first question
+start.click(function() {
+    
+// countdown for 3 seconds before quiz begins
+const firstCountdown = () => {
+    let initialTime = 3;
+    let firstInterval = setInterval(function () {
+        if (initialTime !== 0) {
+            initialTimer.innerHTML = initialTime;
+            initialTime--;
+        } else {
+        initialTimer.textContent = "GO!";
+        clearInterval(firstInterval)
+        }
+    }, 1000);
+}
+    firstCountdown()
+    intro.delay(4000).fadeOut(); // hide intro
+    question.delay(4700).slideDown() //show question and timer
+});
+
+
+
