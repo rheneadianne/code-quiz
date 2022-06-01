fa9b322157914f9fb6df2aa3911817b43d12eaad
rhenea <rheneadianne@gmail.com>
2022-05-31 21:58:00 -0400

    timer works T.T

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,11 +5,12 @@ const intro = $(".intro")
 const questionTitle = $(".question")
 const choices = $(".choices")
 const initialTimer = document.querySelector(".initialTimer")
+const timer = document.querySelector(".timer")
 
 // hides intro, countsdown 3 seconds, then shows first question
 start.click(function() {
     
-// countdown for 3 seconds before quiz begins
+// countdown for 4 seconds before quiz begins
 const firstCountdown = () => {
     let initialTime = 3;
     let firstInterval = setInterval(function () {
@@ -22,9 +23,24 @@ const firstCountdown = () => {
         }
     }, 1000);
 }
+
+const countdown = () => {
+    let timeLeft = 124; // extra 4 seconds for delay
+    let countdownInterval = setInterval(function () {
+        if (timeLeft !== 0) {
+            timer.innerHTML = `Time remaining: ${timeLeft}`;
+            timeLeft--;
+        } else {
+            timer.textContent = "Times Up!";
+            clearInterval(countdownInterval)
+            endQuiz()
+        }
+    }, 1000);
+}
     firstCountdown()
     intro.delay(4000).fadeOut(); // hide intro
     question.delay(4700).slideDown() //show question and timer
+    countdown()
 });
 
 
