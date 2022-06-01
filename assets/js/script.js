d48e3ee71e588c40575fb7270b768a2b586befda
rhenea <rheneadianne@gmail.com>
2022-06-01 00:00:04 -0400

    added questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ const questionTitle = document.querySelector(".question")
 const choices = document.querySelector(".choices ul")
 const initialTimer = document.querySelector(".initialTimer")
 const timer = document.querySelector(".timer")
+let timeLeft = 124; // 2 mins to complete + 4 seconds to account for delay
 
 // hides intro, countsdown 3 seconds, then shows first question
 start.click(function() {
@@ -25,7 +26,6 @@ const firstCountdown = () => {
 }
 
 const countdown = () => {
-    let timeLeft = 124; // extra 4 seconds for delay
     let countdownInterval = setInterval(function () {
         if (timeLeft !== 0) {
             timer.innerHTML = `Time remaining: ${timeLeft}`;
@@ -40,27 +40,83 @@ const countdown = () => {
     firstCountdown()
     intro.delay(4000).fadeOut(); // hide intro
     questionWrap.delay(4700).slideDown() //show question and timer
-    countdown()
-    question()
+    countdown() //start timer
+    question() //call question
 });
 
-// questions as objects -- proof of concept
+// questions as objects
 const questionList = [
     {
         question: "Commonly used Data types do NOT include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with _____.",
+        choices: ["quotation marks", "curly brackets", "parenthesis", "square brackets"],
+        answer: "curly brackets"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotation marks", "parenthesis"],
+        answer: "quotation marks"
+    },
+    {
+        question: "A very useful tool used during developement and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },
+    {
+        question: "In ES6, JavaScript introduced 2 types of variables. What are they called?",
+        choices: ["let and const", "var and let", "var and const", "none of the above"],
+        answer: "let and const"
+    },
+    {
+        question: "Which of the following is a correct way to declare an arrow function",
+        choices: [
+            "function sipJuice (juice, flavour) => ", 
+            "var buyCoffee = '' => ", 
+            "let haveBrunch = food, cost => ", 
+            "const eatFood = foodChoice => "
+        ],
+        answer: "const eatFood = foodChoice => "
     }
 ]
 
+let questionNumber = 0
+
 const question = () => {
-    let questionNumber = 0
+    
     let currentQuestion = questionList[questionNumber] // gets question object from array
     questionTitle.textContent = currentQuestion.question // updates question title
-    for (i = 0; i < currentQuestion.choices.length; i++) {
-        const makeChoices = document.createElement("li")
-        makeChoices.textContent = currentQuestion.choices[i]
-        choices.appendChild(makeChoices)
+
+    for (i = 0; i < currentQuestion.choices.length; i++) { // creates choices
+        const makeChoices = document.createElement("li");
+        makeChoices.setAttribute("class", "choice");
+        makeChoices.textContent = currentQuestion.choices[i];
+        choices.appendChild(makeChoices);
     }
+
+    $(".choice").click(function(){
+        if (this.textContent !== questionList[questionNumber].answer) {
+            timeLeft -= 10 // penalty for wrong answer
+            if (timeLeft < 0) { // to avoid negative time
+                timeLeft = 0
+            }
+        } else {
+            questionNumber++ // next question in array
+            choices.innerHTML = "" // clears last question
+                if (questionNumber === questionList.length) {
+                    endQuiz()
+                } else {
+                    question()
+                }
+        }
+    });
 }
 
